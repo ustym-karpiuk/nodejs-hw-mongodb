b5ed78c6609eadd13a63c7df03d3af78fd6d2700
ustym-karpiuk <karpyuk0502@gmail.com>
2024-10-22 12:18:17 +0200

    v1.1 (add contact db)
    Added contact json, routes to find all contacts and find contact by id. Init mondodb database. Added ContactSchema.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 
+import { getAllContacts, getContactById } from './services/contacts.js';
+
 const PORT = Number(env('PORT', 3000));
 export const setupServer = () => {
   {
@@ -19,6 +21,38 @@ export const setupServer = () => {
       transport: { target: 'pino-pretty' },
     }),
   );
+
+  {
+    /* Routes */
+  }
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Contact not found!',
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id: ${contactId}`,
+      data: contact,
+    });
+  });
 
   {
     /* 404 and 500 */
