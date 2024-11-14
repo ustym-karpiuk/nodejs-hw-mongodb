54c55cabf17bf75245409c14564efee1395c50cb
ustym-karpiuk <karpyuk0502@gmail.com>
2024-11-14 17:49:48 +0100

    v1.3(bugfix)
    Fixed bug when err.detailes not showed when error with patch or post appered.

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,7 +5,7 @@ export const errorHandler = (err, req, res, next) => {
     res.status(err.status).json({
       status: err.status,
       message: err.message,
-      data: null,
+      data: err,
     });
     return;
   }
